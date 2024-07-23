b0a9754978f391ffd9349eaaf32d0e4f44ac1a89
asr09122 <116746723+asr09122@users.noreply.github.com>
2024-07-23 23:30:49 +0530

    Time: 81 ms (76.57%), Space: 96.3 MB (10.08%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -4,7 +4,7 @@ public:
         int buy = prices[0];
         int profit = 0;
 
-        for (int i = 1; i < prices.size(); ++i) {
+        for (int i = 1; i < prices.size(); i++) {
             if (prices[i] < buy) {
                 buy = prices[i];
             } else if (prices[i] - buy > profit) {
